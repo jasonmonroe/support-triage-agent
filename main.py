02cd04484aefaebe7473c140a4fe2ff4d2cd321b
Jason Monroe <jwmonroe1@gmail.com>
2026-08-13 22:17:46 -0500

    adding basic files for agent

---
+++ main.py
@@ -0,0 +1,14 @@
+# main.py
+
+"""
+
+"""
+
+def run_main_pipeline():
+    pass
+
+def parse_args(command_line_str: str) -> dict:
+    return {}
+
+if __name__ == "__main__":
+    run_main_pipeline()
---
+++ src/constants.py
@@ -0,0 +1,19 @@
+# src/constants.py
+# +---------------------------------------------------------------------------+
+# |                            CONSTANTS                                       |
+# +---------------------------------------------------------------------------+
+
+import os
+
+# General Variables
+MSEC = 1000
+PEP8_LINE_CNT = 79
+
+# Environment Variables
+MODEL_API_KEY = os.getenv("MODEL_API_KEY")
+MODEL_API_URL = os.getenv("MODEL_API_URL")
+MODEL_NAME = os.getenv("MODEL_NAME")
+
+# Prompts
+SYSTEM_PROMPT = """"""
+USER_PROMPT_TEMPLATE = """"""
